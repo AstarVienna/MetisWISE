5518c601701f166460ddb12a4fde34925e8cec92
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2025-05-08 09:49:36 +0200

    Some cleanup

--- metiswise/main/aweimports.py
+++ metiswise/main/aweimports.py
@@ -1,3 +1,6 @@
+# noinspection PyUnresolvedReferences
 from metiswise.main.dataitem import *
+# noinspection PyUnresolvedReferences
 from metiswise.main.hardware import *
+# noinspection PyUnresolvedReferences
 from metiswise.main.raw import *
--- metiswise/main/dataitem.py
+++ metiswise/main/dataitem.py
@@ -2,5 +2,9 @@
 
 from common.database.DataObject import DataObject, persistent
 
+
 class DataItem(DataObject):
     pass
+
+
+__all__ = ['DataItem', 'persistent']
