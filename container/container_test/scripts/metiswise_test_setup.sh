c1047b96fa9218ed41b0e844e219b450a58e58b9
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2025-06-27 21:55:26 +0200

    Add simulation of data

--- container/container_test/scripts/metiswise_test_setup.sh
+++ container/container_test/scripts/metiswise_test_setup.sh
@@ -22,5 +22,20 @@ source "${HOME}/MetisWISE/toolbox/become_normal_user.sh"
 echo "Tell the dbviewer it can start"
 touch "${HOME}/space/control/database_setup"
 
+echo "Going to simulate some data"
+DIR_SITE_PACKAGES=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
+DIR_SIMULATIONS="${DIR_SITE_PACKAGES}/Simulations"
+# YAML="$DIR_SIMULATIONS/YAML/allRecipes.yaml"
+YAML="$DIR_SIMULATIONS/YAML/img.yaml"
+
+ln -s irdb inst_pkgs
+# TODO: Ensure nCores can be set to 10 or so; can cause troubles with pooch.
+python3 "${DIR_SIMULATIONS}/python/run_recipes.py" \
+    --inputYAML="${YAML}" \
+    --outputDir "${HOME}/space/raw" \
+    --doCalib=1 --sequence=1 --doStatic --nCores=1
+
+
+
 echo "Stay a while... stay forever!"
 while true; do sleep 60 ; done
