5709586c2f1a5ebb7880a6fc8a52ab465d3df3e0
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2025-06-27 16:54:21 +0200

    Add compose setup for testing

---
+++ container/container_test/entrypoint_dbview.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# For some reason .bashrc is skipped.
+eval "$(conda shell.bash hook)"
+# TODO: use an actual environment
+conda activate base
+export AWETARGET=metiswise
+# TODO: Move to src directory?
+export PYTHONPATH="${HOME}/MetisWISE"
+"${HOME}/MetisWISE/toolbox/dbview.sh" start
+
+echo "Sleep"
+sleep 10
+echo "Run another bash so the script won't quit"
+bash
