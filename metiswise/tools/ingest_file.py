6688f496fca0e1c5c8e499f1ea21a68baa455a73
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2025-11-26 14:49:41 +0100

    Counting and skip if file is unknown

--- metiswise/tools/ingest_file.py
+++ metiswise/tools/ingest_file.py
@@ -12,6 +12,31 @@ def ingest_file(filename: str):
     print()
     print(f"Ingesting {filename} .")
 
+    to_skip = [
+        "IFU_RSRF_BACKGROUND",
+        "LM_SKY_BASIC_REDUCED",
+        "N_SKY_BASIC_REDUCED",
+
+        # METIS.LM_LSS_SCI_RAW.2027-01-25_00_01_09/2025-11-19T11:30:24.484475/_LM_LSS_SCI_FLUX_TELL_1D.fits .
+        "LM_LSS_SCI_FLUX_TELL_1D",
+
+        # METIS.N_LSS_SCI_RAW.2027-01-25_00_01_09/2025-11-19T11:30:24.484475/_N_LSS_SCI_FLUX_TELL_1D.fits
+        "N_LSS_SCI_FLUX_TELL_1D",
+        
+        # METIS.N_IMAGE_SCI_RAW.2027-01-25_00_01_09/2025-11-19T11:30:24.484475/_N_SCI_BKG.fits
+        "N_SCI_BKG",
+
+        # METIS.N_IMAGE_SCI_RAW.2027-01-25_00_01_09/2025-11-19T11:30:24.484475/_N_STD_BKG.fits
+        "N_STD_BKG",
+
+        # METIS.N_IMAGE_SCI_RAW.2027-01-25_00_01_09/2025-11-19T11:30:24.484475/_N_STD_COMBINED.fits
+        "N_STD_COMBINED",
+    ]
+    for skip in to_skip:
+        if skip in filename:
+            print(f"Skipping {filename}")
+            return
+
     q_di = (DataItem.filename == filename)
     if len(q_di):
         print(f"Found {len(q_di)} existing {filename}.")
@@ -36,8 +61,12 @@ def show_help():
 def main():
     if len(sys.argv) == 1:
         show_help()
+        return
 
-    for filename in sys.argv[1:]:
+    filenames = sys.argv[1:]
+
+    for i, filename in enumerate(filenames):
+        print(f"{i}/{len(filenames)}: {filename}")
         myraw = ingest_file(filename)
         if myraw:
             # TODO: Add some parameter to optionally store / commit.
